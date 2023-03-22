c7937bf782417d37bb93b925dbcb2de2fa4234ea
Admin <Admin@192.168.1.100>
2023-03-22 16:29:03 +0000

    Added unit tests for all cases

--- src/test/java/com/qa/rockpaperscissors/TestRPS.java
+++ src/test/java/com/qa/rockpaperscissors/TestRPS.java
@@ -34,4 +34,108 @@ public class TestRPS {
 		// Check that the mock is being used 10 times
 		Mockito.verify(this.rand, Mockito.times(10)).nextInt(3);
 	}
+
+	@Test
+	void testRockLose() {
+		// Make CPU 'choose' paper
+		Mockito.when(this.rand.nextInt(3)).thenReturn(1);
+		// Test 10 times
+		for (int i = 0; i < 10; i++) {
+			assertEquals(Result.LOSE, this.sim.playGame(Shape.ROCK));
+		}
+
+		// Check that the mock is being used 10 times
+		Mockito.verify(this.rand, Mockito.times(10)).nextInt(3);
+	}
+
+	@Test
+	void testRockTie() {
+		// Make CPU 'choose' rock
+		Mockito.when(this.rand.nextInt(3)).thenReturn(0);
+		// Test 10 times
+		for (int i = 0; i < 10; i++) {
+			assertEquals(Result.TIE, this.sim.playGame(Shape.ROCK));
+		}
+
+		// Check that the mock is being used 10 times
+		Mockito.verify(this.rand, Mockito.times(10)).nextInt(3);
+	}
+
+	@Test
+	void testPaperWin() {
+		// Make CPU 'choose' rock
+		Mockito.when(this.rand.nextInt(3)).thenReturn(0);
+		// Test 10 times
+		for (int i = 0; i < 10; i++) {
+			assertEquals(Result.WIN, this.sim.playGame(Shape.PAPER));
+		}
+
+		// Check that the mock is being used 10 times
+		Mockito.verify(this.rand, Mockito.times(10)).nextInt(3);
+	}
+
+	@Test
+	void testPaperLose() {
+		// Make CPU 'choose' scissors
+		Mockito.when(this.rand.nextInt(3)).thenReturn(2);
+		// Test 10 times
+		for (int i = 0; i < 10; i++) {
+			assertEquals(Result.LOSE, this.sim.playGame(Shape.PAPER));
+		}
+
+		// Check that the mock is being used 10 times
+		Mockito.verify(this.rand, Mockito.times(10)).nextInt(3);
+	}
+
+	@Test
+	void testPaperTie() {
+		// Make CPU 'choose' paper
+		Mockito.when(this.rand.nextInt(3)).thenReturn(1);
+		// Test 10 times
+		for (int i = 0; i < 10; i++) {
+			assertEquals(Result.TIE, this.sim.playGame(Shape.PAPER));
+		}
+
+		// Check that the mock is being used 10 times
+		Mockito.verify(this.rand, Mockito.times(10)).nextInt(3);
+	}
+
+	@Test
+	void testScissorsWin() {
+		// Make CPU 'choose' paper
+		Mockito.when(this.rand.nextInt(3)).thenReturn(1);
+		// Test 10 times
+		for (int i = 0; i < 10; i++) {
+			assertEquals(Result.WIN, this.sim.playGame(Shape.SCISSORS));
+		}
+
+		// Check that the mock is being used 10 times
+		Mockito.verify(this.rand, Mockito.times(10)).nextInt(3);
+	}
+
+	@Test
+	void testScissorsLose() {
+		// Make CPU 'choose' rock
+		Mockito.when(this.rand.nextInt(3)).thenReturn(0);
+		// Test 10 times
+		for (int i = 0; i < 10; i++) {
+			assertEquals(Result.LOSE, this.sim.playGame(Shape.SCISSORS));
+		}
+
+		// Check that the mock is being used 10 times
+		Mockito.verify(this.rand, Mockito.times(10)).nextInt(3);
+	}
+
+	@Test
+	void testScissorsTie() {
+		// Make CPU 'choose' scissors
+		Mockito.when(this.rand.nextInt(3)).thenReturn(2);
+		// Test 10 times
+		for (int i = 0; i < 10; i++) {
+			assertEquals(Result.TIE, this.sim.playGame(Shape.SCISSORS));
+		}
+
+		// Check that the mock is being used 10 times
+		Mockito.verify(this.rand, Mockito.times(10)).nextInt(3);
+	}
 }
